50f9b79b565f13614805ab20ff25dfc70901f7ee
DavidMartinVergues <dmverges@gmail.com>
2021-12-20 07:33:50 +0100

    deleting useless things

--- apps/vcf_manager/api/serializer.py
+++ apps/vcf_manager/api/serializer.py
@@ -7,9 +7,3 @@ class UploadSerializer(serializers.ModelSerializer):
     class Meta():
         model = UploadedFile
         fields = ('file',)
-
-class VariantsSerializer(serializers.ModelSerializer):
-   
-    class Meta():
-        model = Variants
-        fields = '__all__'
